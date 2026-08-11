2e96d9decdd1c12dd20ba82043373a190cdcce1e
marc king <marclawrenceking@gmail.com>
2026-08-11 15:26:39 +0800

    Add 'rate_per_hr' column to companies table and update Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['name', 'is_active'])]
+#[Fillable(['name', 'is_active', 'rate_per_hr'])]
 class Company extends Model
 {
     /**
@@ -19,6 +19,7 @@ class Company extends Model
     {
         return [
             'is_active' => 'boolean',
+            'rate_per_hr' => 'decimal:2',
         ];
     }
 
